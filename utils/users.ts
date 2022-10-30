6b682666cc4f8a618456b9392bea51cb45b51282
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-10-30 18:00:32 +0100

    api tests

--- utils/users.ts
+++ utils/users.ts
@@ -1,12 +1,22 @@
 import { faker } from '@faker-js/faker';
+import { Roles } from './roles';
 
-export const getRandomUser = () => {
+type User = {
+    firstName: string,
+    lastName: string,
+    username: string,
+    password: string,
+    roles: Roles[],
+    email: string
+}
+
+export const getRandomUser = (): User => {
     return {
         firstName: faker.name.firstName(),
         lastName: faker.name.lastName(),
         username: faker.internet.userName(),
         password: faker.internet.password(),
-        roles: [ 'ROLE_CLIENT', 'ROLE_ADMIN' ],
+        roles: [ Roles.ROLE_ADMIN, Roles.ROLE_CLIENT ],
         email: faker.internet.email()
     }
 }
